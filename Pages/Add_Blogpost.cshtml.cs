bd6497e9c52fcd36da5a80436c437898d3ce74d3
Alboo <albert.oue@hotmail.com>
2020-11-06 17:34:24 -0500

    Added new pages, added them to the Nav bar, added some text on the index so it's not fully vanilla.

---
+++ Pages/Add_Blogpost.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Alblog.Pages
+{
+    public class Add_BlogpostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
