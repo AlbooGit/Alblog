3c6d2f6ecde3d4c1cfc89bae4889553e309ff84e
Alboo <albert.oue@hotmail.com>
2020-11-06 17:15:29 -0500

    Adding a base file for a blogpost before using branches

--- Models/Blogpost.cs
+++ Models/Blogpost.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Alblog.Models
 {
     public class Blogpost
     {
+        public string Id { get; set; }
+        public string UserPosting { get; set; }
+        public string Title { get; set; }
+        public string PostContent { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Blogpost>(this);
     }
 }
