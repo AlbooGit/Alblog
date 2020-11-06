1915a0e3aa16550364278b1c55944a247709833a
Alboo <albert.oue@hotmail.com>
2020-11-06 17:25:00 -0500

    Changed userPosting to author because makes more sense. Added date and possibility of attaching and image to a blogpost

--- Models/Blogpost.cs
+++ Models/Blogpost.cs
@@ -9,7 +9,9 @@ namespace Alblog.Models
     public class Blogpost
     {
         public string Id { get; set; }
-        public string UserPosting { get; set; }
+        public string Author { get; set; }
+        public string Date { get; set; }
+        public string Image { get; set; }
         public string Title { get; set; }
         public string PostContent { get; set; }
         public override string ToString() => JsonSerializer.Serialize<Blogpost>(this);
